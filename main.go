fcae92d252911936daf1c3c9463b0d29b757cb81
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2025-11-11 09:32:01 +0100

    todolist

--- main.go
+++ main.go
@@ -6,6 +6,16 @@ type Token struct {
 	value int
 }
 
+// TODO: what will i do
+// remove generics
+// - [ ] Make it a way to create your own Tokenkind as an int
+// - [ ] Make helper functions for establishing tokens
+// - [ ] Rune indexing into array instead of hashmap
+// - [ ] Create a default lexer which registers C like things?
+// - [ ] Add a way to provide keywords, and what tokentype they should produce
+// 			"KEYWORD", KEYWORD (constant)
+// - [ ] think about error handling. how will this be handled and returned?
+
 func main() {
 
 	lex := lexer.NewLexer(
